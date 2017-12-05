8a36f01d8695d50fdff609cfb7570f4b2eb67fd9
Ethan <ethanahum@me.com>
2017-12-05 10:10:18 +0200

    commit

---
+++ tabBarController/tabBarController/RedViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  tabBarController
+//
+//  Created by Administrateur on 21/11/2017.
+//  Copyright © 2017 Administrateur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        tabBarItem.badgeValue = "!"
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
